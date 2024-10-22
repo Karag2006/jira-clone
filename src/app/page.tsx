5b8394b787ecb5b11bd913fe8988cbe7ac6d5199
Martin <karag2006@gmx.net>
2024-10-22 23:01:07 +0200

    Add all Shadcn Components and modify: Button, Input and Select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <Button variant="destructive">Click me</Button>
-      <p className=" text-red-500 font-semibold">Test</p>
+      <div className="flex gap-4 m-4">
+        <Button>Primary</Button>
+
+        <Button variant="secondary">Secondary</Button>
+
+        <Button variant="destructive">Destructive</Button>
+
+        <Button variant="ghost">Ghost</Button>
+
+        <Button variant="muted">Muted</Button>
+
+        <Button variant="outline">Outline</Button>
+
+        <Button variant="teritary">Teritary</Button>
+      </div>
+      <div>
+        <Input />
+      </div>
     </div>
   );
 };
