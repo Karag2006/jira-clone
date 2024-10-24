ec4c3d5fd0c7326c70901684e2cd7f6b6d320fd4
Martin <karag2006@gmx.net>
2024-10-24 22:56:25 +0200

    Sign In / Sign Up Forms

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,14 +1,20 @@
+import Link from "next/link";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-
-import { DottetSeperator } from "@/components/dottet-seperator";
-import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
@@ -16,17 +22,23 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import Link from "next/link";
+
+import { DottetSeperator } from "@/components/dottet-seperator";
 
 const formSchema = z.object({
+  name: z.string().trim().min(1, "You need to enter your name"),
   email: z.string().email(),
-  password: z.string().min(1, "You need to enter your password").max(256),
+  password: z
+    .string()
+    .min(8, "You need to set a password with at least 8 Characters")
+    .max(256),
 });
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: "",
       email: "",
       password: "",
     },
@@ -38,7 +50,12 @@ export const SignInCard = () => {
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
-        <CardTitle className="text-2xl">Welcome back!</CardTitle>
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          By signing up, you agree to our{" "}
+          <Link href="/terms-of-service">Terms of Service</Link> and{" "}
+          <Link href="/privacy">Privacy Policy</Link>
+        </CardDescription>
       </CardHeader>
       <div className="px-7">
         <DottetSeperator />
@@ -46,6 +63,22 @@ export const SignInCard = () => {
       <CardContent className="p-7">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="Enter your Name"
+                    />
+                  </FormControl>
+                  <FormMessage {...field} />
+                </FormItem>
+              )}
+            />
             <FormField
               name="email"
               control={form.control}
@@ -80,7 +113,7 @@ export const SignInCard = () => {
             />
 
             <Button type="submit" size="lg" className="w-full" disabled={false}>
-              Login
+              Sign Up
             </Button>
           </form>
         </Form>
@@ -113,7 +146,7 @@ export const SignInCard = () => {
       </div>
       <CardContent className="p-7 flex items-center justify-center">
         <p>
-          Don&apos;t have an account? <Link href="/sign-up">Sign Up</Link>
+          Already have an account? <Link href="/sign-in">Login</Link>
         </p>
       </CardContent>
     </Card>
