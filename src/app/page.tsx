4f177a314f3638aab99fad8aa296a06a5bc5e951
Martin <karag2006@gmx.net>
2024-11-04 23:00:33 +0100

    User Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
+import { UserButton } from "@/features/auth/components/user-button";
+
 import { useRouter } from "next/navigation";
 
 import { useEffect } from "react";
@@ -10,7 +10,6 @@ import { useEffect } from "react";
 const Home = () => {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
 
   useEffect(() => {
     if (!data && !isLoading) {
@@ -20,8 +19,7 @@ const Home = () => {
 
   return (
     <div>
-      <div>Authorized only</div>
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 };
