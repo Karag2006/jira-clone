582c33ec803f02db92a78f0529f4f1caf63efcce
Martin <karag2006@gmx.net>
2024-11-07 23:44:24 +0100

    Workspace Form.

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -7,33 +7,81 @@ import { z } from "zod";
 import { createWorkspaceSchema } from "../schemas";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DottetSeperator } from "@/components/dottet-seperator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspace";
 
 interface CreateWorkspaceFormProps {
-	onCancel?: () => void;
+  onCancel?: () => void;
 }
 
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
-	const form = useForm<z.infer<typeof createWorkspaceSchema>>({
-		resolver: zodResolver(createWorkspaceSchema),
-		defaultValues: {
-			name: "",
-		},
-	});
+  const { mutate, isPending } = useCreateWorkspace();
 
-	const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
-		console.log(values);
-	};
-	return (
-		<Card className='w-full h-full border-none shadow-none'>
-			<CardHeader className='flex p-7'>
-				<CardTitle className='text-xl font-bold'>
-					Create a new Workspace
-				</CardTitle>
-			</CardHeader>
-			<div className='px-7'>
-				<DottetSeperator />
-			</div>
-			<CardContent className='p-7'></CardContent>
-		</Card>
-	);
+  const form = useForm<z.infer<typeof createWorkspaceSchema>>({
+    resolver: zodResolver(createWorkspaceSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
+    mutate({ json: values });
+  };
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="flex p-7">
+        <CardTitle className="text-xl font-bold">
+          Create a new Workspace
+        </CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottetSeperator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Workspace Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter Wprkspace Name" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DottetSeperator className="py-7" />
+            <div className="flex items-center justify-between">
+              <Button
+                type="button"
+                variant="secondary"
+                size="lg"
+                onClick={onCancel}
+                disabled={isPending}
+              >
+                Cancel
+              </Button>
+              <Button size="lg" type="submit" disabled={isPending}>
+                Create Workspace
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
 };
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -8,24 +8,24 @@ import { DATABASE_ID, WORKSPACES_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono().post(
-	"/",
-	zValidator("json", createWorkspaceSchema),
-	sessionMiddleware,
-	async (c) => {
-		const databases = c.get("databases");
-		const user = c.get("user");
+  "/",
+  zValidator("json", createWorkspaceSchema),
+  sessionMiddleware,
+  async (c) => {
+    const databases = c.get("databases");
+    const user = c.get("user");
 
-		const { name } = c.req.valid("json");
+    const { name } = c.req.valid("json");
 
-		const workspace = await databases.createDocument(
-			DATABASE_ID,
-			WORKSPACES_ID,
-			ID.unique(),
-			{ name }
-		);
+    const workspace = await databases.createDocument(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      ID.unique(),
+      { name, userId: user.$id }
+    );
 
-		return c.json({ data: workspace });
-	}
+    return c.json({ data: workspace });
+  }
 );
 
 export default app;
