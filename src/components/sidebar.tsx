f4cba36a564c4bd587d5dc1e92654715a5a3bbd6
Martin <karag2006@gmx.net>
2024-11-14 00:05:53 +0100

    get workspaces

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { DottetSeperator } from "./dottet-seperator";
 import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Siedebar = () => {
-	return (
-		<aside className='h-full bg-neutral-100 p-4 w-full'>
-			<Link href='/'>
-				<Image src='/logo.svg' alt='Logo' width={84} height={84} />
-			</Link>
-			<DottetSeperator className='my-4' />
-			<Navigation />
-		</aside>
-	);
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        <Image src="/logo.svg" alt="Logo" width={84} height={84} />
+      </Link>
+      <DottetSeperator className="my-4" />
+      <WorkspaceSwitcher />
+      <DottetSeperator className="my-4" />
+      <Navigation />
+    </aside>
+  );
 };
